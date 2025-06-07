6f137bac10361fbc09c22eb76721f67dd47d9c60
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-06-08 03:06:29 +0530

    Login page added

--- app/dashboard/business-details/page.tsx
+++ app/dashboard/business-details/page.tsx
@@ -5,8 +5,6 @@ const BusinessDetails = () => {
     <div>
       <div className="flex flex-1 flex-col gap-4 p-4">
         <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
-        <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
-        <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
         <div className="bg-muted/50 mx-auto h-[100vh] w-full max-w-3xl rounded-xl" />
       </div>
     </div>
