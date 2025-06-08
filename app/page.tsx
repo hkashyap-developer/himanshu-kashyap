17498f62a67604c1e929fddef447bd9cd194857c
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-06-09 05:17:17 +0530

    changes
    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import CardGrid from "@/components/custom/global/CardGrid";
 import Footer from "@/components/custom/global/footer";
 import Header from "@/components/custom/global/Header/header";
+import HeroSlider from "@/components/custom/HeroSlider/HeroSlider";
 import HomeHero from "@/components/custom/global/HomeHero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HomeHero />
+      <HeroSlider />
+
       <CardGrid />
       <Footer />
     </>
