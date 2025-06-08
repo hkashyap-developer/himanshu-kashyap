70fbd1facc42da6c58ed9992d577c085a69d942f
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-06-08 19:00:14 +0530

    header udpate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import CardGrid from "@/components/custom/global/CardGrid";
 import Footer from "@/components/custom/global/footer";
-import Header from "@/components/custom/global/header";
+import Header from "@/components/custom/global/Header/header";
 import HomeHero from "@/components/custom/global/HomeHero";
 
 export default function Home() {
--- components/custom/global/Header/header.tsx
+++ components/custom/global/Header/header.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 
@@ -11,8 +13,13 @@ export default function Header() {
   return (
     <header className="w-full bg-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
-        <h1 className="text-2xl font-bold">Bot X</h1>
-
+        <Image
+          src="https://i.ibb.co/spPh6DZ7/Thought-2-1.png"
+          alt="Build XI Logo"
+          width={160}
+          height={400}
+          className=""
+        />
         <nav className="hidden md:flex gap-6">
           {navItems.map((item) => (
             <a
@@ -24,7 +31,7 @@ export default function Header() {
             </a>
           ))}
         </nav>
-
+        test
         <button
           className="md:hidden text-gray-700"
           onClick={() => setMenuOpen(!menuOpen)}
@@ -32,7 +39,6 @@ export default function Header() {
           {menuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
       </div>
-
       {menuOpen && (
         <div className="md:hidden bg-white px-4 pb-4">
           {navItems.map((item) => (
